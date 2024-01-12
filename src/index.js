6eb8b16bcf2a2b63c8d06f29d2c0c1c8e3e507be
Juan <dev@juanaleman.com>
2024-01-12 16:29:24 -0600

    v0.0.1 return html and favicon - favicon source: https://counterparty.io/wp-content/uploads/2015/01/favicon.ico

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 const port = 3000;
 
 app.get('/', async (req, res) => {
-	res.status(200).json({
-		wip: true,
-	});
+	const html = `
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>xcp.dev</h1>
+<p>server side rendered proof of concept</p>
+
+</body>
+</html>
+	`;
+
+	res.set('Content-Type', 'text/html');
+	res.status(200);
+	res.send(Buffer.from(html.trim()));
+});
+
+app.get('/favicon.ico', async (req, res) => {
+	res.sendFile(path.join(__dirname, '/favicon.ico'));
 });
 
 app.listen(port, () => {
